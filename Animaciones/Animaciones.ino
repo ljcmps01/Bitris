3f120e1eab732f58b3389f3bfaa60664ce3a051e
ljcmps <46141665+ljcmps01@users.noreply.github.com>
2019-04-16 23:56:17 -0300

    Programa con las funciones de animacion del juego

---
+++ Animaciones/Animaciones.ino
@@ -0,0 +1,88 @@
+#define COL PORTK
+#define FILA PORTF
+
+const int out[7]={1,2,4,8,16,32,64};
+bool hab[7][5]={
+{1,1,1,1,1},
+{1,1,1,1,1},
+{1,1,1,1,1},
+{1,1,1,1,1},
+{1,1,1,1,1},
+{1,1,1,1,1},
+{1,1,1,1,1}
+};
+volatile int fila=0,col=0;
+
+void VerticalEdit(int y,bool value);
+void HorizontalEdit(int x,bool value);
+void Clear();
+void VerticalClean();
+
+void setup() {
+ 
+  DDRF=255;
+  FILA=1;  
+  DDRK=255;
+  COL=255;
+  Serial.begin(9600);
+  cli();                              //deshabilito temporalmente las interrupciones
+  TCCR1A=0;                           //limpiamos los registros de control
+  TCCR1B=0;                           //del timer 1
+
+  OCR1A=5;            //3 valor minimo, 5 se ve multiplexado en grabacion de 60fps, 8 valor maximo
+  TCCR1B|=(1<<WGM12);
+  TCCR1B|=(1<<CS10);
+  TCCR1B|=(1<<CS12);
+  
+  
+  TIMSK1|=(1<<OCIE1A);                  //enciendo el timer1
+  
+  sei();                              //activamos las interrupciones
+
+}
+
+void loop() {
+  VerticalClean();
+}
+
+ISR(TIMER1_COMPA_vect){                 //ISR es la rutina de interrupcion
+  if(hab[fila][col])
+  COL=~(1<<col);
+  else COL=~(0<<col);
+  FILA=(1<<fila);
+  if(col<5)col++;
+  else{
+    col=0;
+    if(fila<6)fila++;
+    else fila=0;
+  }
+}
+
+void VerticalEdit(int y,bool value){
+  int i;
+  for(i=0;i<5;i++){
+    hab[y][i]=value;
+  }
+}
+
+void HorizontalEdit(int x,bool value){
+  int i;
+  for(i=0;i<7;i++){
+    hab[i][x]=value;
+  }
+}
+
+void Clear(){
+  int i;
+  for(i=0;i<5;i++){
+    HorizontalEdit(i,0);
+  }
+}
+
+void VerticalClean(){
+  int i;
+  for(i=0;i<7;i++){
+    VerticalEdit(i,0);
+    delay(100);
+  }
+}
